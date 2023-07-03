c562ff1928ddfe0beb64aa5622ddc1aa107cf268
melih ışık <121313959+hilem3002@users.noreply.github.com>
2023-07-03 20:24:45 +0300

    new java file
    new java file for account verification activity

---
+++ main/java/com/example/studooo_v1/verifyepostaActivity.java
@@ -0,0 +1,14 @@
+package com.example.studooo_v1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class verifyepostaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.verifyeposta_design);
+    }
+}
