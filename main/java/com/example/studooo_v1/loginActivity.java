0d09ab0efb7c7e0c717bdaca9593ecfa0e981b2d
melih ışık <121313959+hilem3002@users.noreply.github.com>
2023-07-13 21:17:57 +0300

    Update loginActivity.java
    added imports

--- main/java/com/example/studooo_v1/loginActivity.java
+++ main/java/com/example/studooo_v1/loginActivity.java
@@ -1,6 +1,12 @@
 package com.example.studooo_v1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
