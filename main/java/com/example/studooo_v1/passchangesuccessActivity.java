3f7ec40ad80022e091f49a3d4f75193097b9f24d
melih ışık <121313959+hilem3002@users.noreply.github.com>
2023-07-13 21:19:07 +0300

    Update passchangesuccessActivity.java
    added activity

--- main/java/com/example/studooo_v1/passchangesuccessActivity.java
+++ main/java/com/example/studooo_v1/passchangesuccessActivity.java
@@ -1,14 +1,34 @@
 package com.example.studooo_v1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class passchangesuccessActivity extends AppCompatActivity {
 
+    Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.passchangesuccess_design);
+
+        this.loginButton = findViewById(R.id.loginButton);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // pass change succeeded and go to login page
+                Intent intent = new Intent(passchangesuccessActivity.this, loginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
     }
 }
